9789ae10182d8cdeab7cb79bd04363a38abcfe39
nbuechler <natebuechler@gmail.com>
2014-12-11 00:59:08 -0500

    Added a jslinted javascript called densityScope.js with some helped methods

--- js/densityScope.js
+++ js/densityScope.js
@@ -1,8 +1,50 @@
-(function () {
+var denScop = (function dScope() {
     'use strict';
 
-    var densityScope = function densityScope() {
-        return 'hello';
-    };
+    //Parsers
+    var i, j, phrase,
+        parseSpace = function (t) {
+            return t.split(" ");
+        },
+        parseShortDash = function (t) {
+            return t.split("-");
+        },
+        parseLongDash = function (t) {
+            return t.split("–");
+        },
+        parseAll = function (t) {
+            var parsedText = [];
+            /*jslint plusplus: true */
+            for (i = 0; parseLongDash(t).length > i; i++) {
+                /*
+                 * Parses the long dashes out into a phrase to be parsed
+                 */
+                phrase = parseLongDash(t)[i];
+                for (j = 0; parseSpace(phrase).length > j; j++) {
+                    /*
+                     * Parses the spaces into a word to be parsed
+                     */
+                    if (parseSpace(phrase)[j] !== "") {
+                        parsedText.push(parseSpace(phrase)[j]);
+                    }
+                }
+            }
+            return parsedText;
+        };
     
+    return {
+        version: "0.0.2",
+        parseSpace: parseSpace,
+        parseShortDash: parseShortDash,
+        parseLongDash: parseLongDash,
+        parseAll: parseAll,
+        greeting: function () {
+            return 'Hello';
+        },
+        //Not certain if this will keep
+        nameGenerator: function (name) {
+            return name;
+        }
+    };
+
 }());
